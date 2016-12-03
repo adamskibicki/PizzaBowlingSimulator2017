4bc11f39dea2e67bece6e704d602e20ee1c2d96b
Adam Skibicki Home PC <adam-skibicki@outlook.com>
2016-12-03 14:27:48 +0100

    commit z podstawowymi skadnikami

---
+++ PizzaBowlingSimulator2017/Assets/Scripts/SkladnikThrow.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkladnikThrow : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.tag == "Pizza")
+        {
+            Destroy(GetComponent<Collider>());
+            Destroy(GetComponent<Rigidbody>());
+            Destroy(this);
+        }
+    }
+}
---
+++ PizzaBowlingSimulator2017/Assets/SkladnikController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SkladnikController : MonoBehaviour
+{
+    public static SkladnikController Instance;
+
+    public List<GameObject> skladniki;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+}
