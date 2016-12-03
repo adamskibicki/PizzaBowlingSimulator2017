e5c3f567a4690f829812753766a10d1b079dcf38
Adam Skibicki Home PC <adam-skibicki@outlook.com>
2016-12-03 12:39:18 +0100

    Add ball throw trigger controller

---
+++ PizzaBowlingSimulator2017/Assets/Scripts/BallThrowTriggerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class BallThrowTriggerController : MonoBehaviour
+{
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ThrowBall();
+    }
+
+    private void ThrowBall()
+    {
+
+    }
+}
