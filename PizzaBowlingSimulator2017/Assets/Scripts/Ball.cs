362de2bcfa028c751b4e33c1c3e4c0eb95bdd586
JOHNNY-ACER-ASP\acer <jankow97@gmail.com>
2016-12-03 15:35:57 +0100

    Add ball throwing

--- PizzaBowlingSimulator2017/Assets/Scripts/Ball.cs
+++ PizzaBowlingSimulator2017/Assets/Scripts/Ball.cs
@@ -3,5 +3,41 @@ using System.Collections;
 
 public class Ball : MonoBehaviour
 {
+    [HideInInspector]
+    public bool isGrabbed = false;
 
+    private Vector3 lastFramePosition;
+    private float lastFrameTime;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(!isGrabbed && col.gameObject.CompareTag("Hand"))
+        {
+            Grab(col.gameObject);
+        }
+        if(isGrabbed && col.gameObject.CompareTag("ThrowTrigger"))
+        {
+            Throw();
+        }
+    }
+
+    void Grab(GameObject go)
+    {
+        gameObject.transform.parent = go.gameObject.transform;
+    }
+
+    void Throw()
+    {
+        gameObject.transform.parent = null;
+        float time = Time.deltaTime - lastFrameTime;
+        Vector3 distance = transform.position - lastFramePosition;
+        Vector3 force = distance * time * 100;
+        gameObject.GetComponent<Rigidbody>().AddForce(force, ForceMode.Impulse);
+    }
+
+    void Update()
+    {
+        lastFrameTime = Time.deltaTime;
+        lastFramePosition = transform.position;
+    }
 }
