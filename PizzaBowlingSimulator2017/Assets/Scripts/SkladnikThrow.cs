1369e4efe80d4e65bc27a779b129ea13881f0121
Adam Skibicki Home PC <adam-skibicki@outlook.com>
2016-12-03 18:50:39 +0100

    Add scripts to skladniki

--- PizzaBowlingSimulator2017/Assets/Scripts/SkladnikThrow.cs
+++ PizzaBowlingSimulator2017/Assets/Scripts/SkladnikThrow.cs
@@ -3,11 +3,19 @@ using System.Collections;
 
 public class SkladnikThrow : MonoBehaviour
 {
+    public static GameObject pizza;
+
+    private void Awake()
+    {
+        pizza = GameObject.Find("pizzaCylinde");
+    }
+
     private void OnCollisionEnter(Collision other)
     {
         if (other.collider.tag == "Pizza")
         {
             Destroy(GetComponent<Collider>());
+            gameObject.layer = LayerMask.NameToLayer("Skladnik");
             Destroy(GetComponent<Rigidbody>());
             Destroy(this);
         }
