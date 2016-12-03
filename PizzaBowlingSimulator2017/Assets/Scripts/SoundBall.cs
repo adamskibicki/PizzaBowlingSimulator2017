95b85059a8ad7dc93fab0d636a8cef539101812e
seluss <adam.selus@gmail.com>
2016-12-03 17:17:38 +0100

    Dzwieki

---
+++ PizzaBowlingSimulator2017/Assets/Scripts/SoundBall.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundBall : MonoBehaviour {
+
+	public GameObject bowlingSound;
+	private AudioSource audioSource;
+
+	private void OnCollisionEnter(Collision other)
+	{
+		if (other.collider.tag == "Track")
+		{
+			audioSource = bowlingSound.GetComponent<AudioSource>();
+			audioSource.Play ();
+		}
+	}
+}
