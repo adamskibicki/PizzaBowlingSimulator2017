6dfb94bf1186e671f1e2c118b5bef3f646f4c64c
JOHNNY-ACER-ASP\acer <jankow97@gmail.com>
2016-12-03 19:57:42 +0100

    asdfasdgfasgasfg

--- PizzaBowlingSimulator2017/Assets/Scripts/PizzaPosReset.cs
+++ PizzaBowlingSimulator2017/Assets/Scripts/PizzaPosReset.cs
@@ -5,7 +5,8 @@ public class PizzaPosReset : MonoBehaviour
 {
     void Awake()
     {
+        
         GameObject pizza = GameObject.FindGameObjectWithTag("Pizza");
-        pizza.transform.position = new Vector3(146.4408f, 25.02513f, 578.7639f);
+        pizza.transform.position = GameObject.FindGameObjectWithTag("GameController").transform.position;
     }
 }
