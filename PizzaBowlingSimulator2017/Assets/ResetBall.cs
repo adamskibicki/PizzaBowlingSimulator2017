38f8da4c6f706ced621418f0a8ed0590118dbc37
Adam Skibicki Home PC <adam-skibicki@outlook.com>
2016-12-03 17:25:15 +0100

    Add controller reset ball

---
+++ PizzaBowlingSimulator2017/Assets/ResetBall.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using VRTK;
+
+public class ResetBall : MonoBehaviour
+{
+    [SerializeField]
+    private SteamVR_TrackedController controllerLeft;
+    [SerializeField]
+    private SteamVR_TrackedController controllerRight;
+
+
+    // Use this for initialization
+    private void OnEnable()
+    {
+        controllerLeft.PadClicked += reset;
+        controllerRight.PadClicked += reset;
+    }
+
+    private void OnDisable()
+    {
+        controllerLeft.PadClicked -= reset;
+        controllerRight.PadClicked -= reset;
+    }
+
+    private void reset(object sender, ClickedEventArgs e)
+    {
+        transform.position = new Vector3(0.887f, 0.725f, -14.47f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
