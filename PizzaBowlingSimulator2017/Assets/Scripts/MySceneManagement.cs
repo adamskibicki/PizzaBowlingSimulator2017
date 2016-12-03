9704100a5d9d0326a956e922daa8b3d76d0d8557
JOHNNY-ACER-ASP\acer <jankow97@gmail.com>
2016-12-03 19:07:35 +0100

    Add SceneManagement + some additional fixes.

---
+++ PizzaBowlingSimulator2017/Assets/Scripts/MySceneManagement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MySceneManagement : MonoBehaviour
+{
+    void ChangeScene()
+    {
+        if (SceneManager.GetActiveScene().name == "Final3")
+        {
+            SceneManager.LoadScene("LookingAtPizza");
+        }
+        else
+        {
+            Destroy(GameObject.FindGameObjectWithTag("Pizza"));
+            SceneManager.LoadScene("Final3");
+        }
+    }
+}
---
+++ PizzaBowlingSimulator2017/Assets/Scripts/PizzaPosReset.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PizzaPosReset : MonoBehaviour
+{
+    void Awake()
+    {
+        GameObject pizza = GameObject.FindGameObjectWithTag("Pizza");
+        pizza.transform.position = new Vector3(146.4408f, 25.02513f, 578.7639f);
+    }
+}
